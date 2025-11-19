65de6196af7f9b773dc3dcd3d16d7d44c7ab3dbd
Yang Yun <agile.yang@gmail.com>
2025-11-19 17:57:27 +0800

    add url probe to PCP

--- pc-pcp/src/main/java/com/cloud/pc/pulse/PulseTask.java
+++ pc-pcp/src/main/java/com/cloud/pc/pulse/PulseTask.java
@@ -17,26 +17,34 @@
 package com.cloud.pc.pulse;
 
 import com.cloud.pc.config.Envs;
+import com.cloud.pc.model.PmsInfo;
 import com.cloud.pc.scanner.DirectoryScanner;
 import com.cloud.pc.model.PcpPulseInfo;
 import com.cloud.pc.scanner.FileStat;
+import com.cloud.pc.utils.FileUtils;
 import com.cloud.pc.utils.HttpUtils;
 import com.cloud.pc.utils.JsonUtils;
 import com.cloud.pc.utils.NetUitls;
 import com.cloud.pc.utils.SecretUtils;
+import com.cloud.pc.utils.UrlProbe;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
-public class PulseTask implements Runnable{
+public class PulseTask implements Runnable, UrlProbe.IUrlProbeFunction{
     private static final Logger LOG = LoggerFactory.getLogger(PulseTask.class);
     private DirectoryScanner directoryScanner;
+    private UrlProbe urlProbe;
 
     public PulseTask(DirectoryScanner directoryScanner) {
         this.directoryScanner = directoryScanner;
+        this.urlProbe = new UrlProbe(Envs.pmsUrl, this);
     }
 
     private static Map<String, String> getPmsHeader() {
@@ -49,13 +57,14 @@ public class PulseTask implements Runnable{
 
     @Override
     public void run() {
+        String url = "";
         try {
             FileStat stat = directoryScanner.getUsage();
             if (stat == null) {
                 LOG.info("PCP is scanning");
                 return;
             }
-            String url = Envs.pmsUrl + "api/v1/pms/pcp/pulse";
+            url = FileUtils.mergePath(urlProbe.getUrl(), "api/v1/pms/pcp/pulse");
             PcpPulseInfo pulseInfo = new PcpPulseInfo();
             pulseInfo.setHost(Envs.httpHeader + NetUitls.getIp() + ":" + Envs.port + "/");
             pulseInfo.setTotalSize(Envs.availableSize);
@@ -70,7 +79,38 @@ public class PulseTask implements Runnable{
                 LOG.error("failed to send pulse info! error:{}" , response.getStatusCode());
             }
         } catch (IOException e) {
+            urlProbe.reportFail(url);
             LOG.error("failed to send pulse info with exception" , e);
         }
+    }
+
+    public List<PmsInfo> getPmsApi(String url) {
+        StringBuilder path = new StringBuilder(FileUtils.mergePath(url,"/api/v1/pms/list"));
+        try {
+            HttpUtils.HttpResponse response = HttpUtils.sendRequest(path.toString(),
+                    "GET", getPmsHeader(), null, null);
+            if (response.getStatusCode() == 200) {
+
+                List<PmsInfo> pmsInfos = JsonUtils.parseList(response.getBody(), PmsInfo.class);
+                LOG.info("got pms list:{} ", pmsInfos);
+                return pmsInfos;
+            } else {
+                LOG.error("failed to get pms list! error:{}", response.getStatusCode());
+            }
+        } catch (IOException e) {
+            LOG.error("exception to get pms list!", e);
+        }
+        return null;
+    }
+
+    public List<String> getUrlList(String url) {
+        List<PmsInfo> pmsInfos = getPmsApi(url);
+
+        if (pmsInfos == null || pmsInfos.isEmpty()) {
+            return Collections.emptyList();
+        }
+        return pmsInfos.stream()
+                .map(PmsInfo::getHost)
+                .collect(Collectors.toList());
     }
 }
